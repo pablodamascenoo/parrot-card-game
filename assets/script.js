686ffda058473ff994ba596c04fba8ea22bacb86
pablodamascenoo <pablofacex@gmail.com>
2022-02-01 12:35:55 -0300

    append cards in .content with javascript

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
 let amountCards = 0
+let content = document.querySelector("section")
 
-
-while(amountCards<4 || amountCards > 14){
+while(amountCards<4 || amountCards > 14 || amountCards%2 != 0){
     amountCards = prompt("Digite a quantidade de cartas do jogo (entre 4 e 14):")
 }
 
-console.log(amountCards)
+for(let i=0; i<amountCards; i++){
+    var card = document.createElement("div")
+    card.classList.add("card")
+    var frontFace = document.createElement("div")
+    frontFace.classList.add("front-face", "face")
+    frontFace.innerText = "Frente"
+    var backFace = document.createElement("div")
+    backFace.classList.add("back-face", "face")
+    backFace.innerText = "Verso"
+    card.appendChild(frontFace)
+    card.appendChild(backFace)
+    content.appendChild(card)
+}
